a0112860f17278a79972f6804b1dedd141a70fdc
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-08 15:36:06 -0300

    Layout front-end

--- coffee-delivery/src/pages/OrderCompleted/index.tsx
+++ coffee-delivery/src/pages/OrderCompleted/index.tsx
@@ -1,9 +1,12 @@
+import { Clock, CurrencyDollar, MapPin } from "phosphor-react";
+import { useTheme } from "styled-components";
 import avatarImg from "../../assets/avatar.svg";
+import { InforWithIcon } from "../../components/InforWithIcon";
 import { RegularText, TitleText } from "../../components/Typography";
 import { OrderCompletedContainer, OrderDetailsContainer } from "./styles";
 
 export function OrderCompleted() {
-
+  const { colors } = useTheme()
   return (
     <OrderCompletedContainer className="container">
       <div>
@@ -12,7 +15,48 @@ export function OrderCompleted() {
       </div>
 
       <section>
-        <OrderDetailsContainer></OrderDetailsContainer>
+        <OrderDetailsContainer>
+
+          <InforWithIcon
+            icon={
+              <MapPin weight="fill" />}
+            text={
+              <RegularText>
+                Entrega em Farrapos <strong>Rua João Daniel Martinelli, 102 </strong>
+                <br />
+                Porto Alegre, RS
+              </RegularText>
+            }
+            iconBg={colors["brand-purple"]}
+          />
+
+          <InforWithIcon
+            icon={
+              <Clock weight="fill" />}
+            text={
+              <RegularText>
+                Previsão de entrega
+                <br />
+                <strong>20 min - 30 min</strong>
+              </RegularText>
+            }
+            iconBg={colors["brand-yellow"]}
+          />
+
+          <InforWithIcon
+            icon={
+              <CurrencyDollar weight="fill" />}
+            text={
+              <RegularText>
+                Pagamento na entrega
+                <br />
+                <strong>Cartão de Crédito</strong>
+              </RegularText>
+            }
+            iconBg={colors["brand-yellow-dark"]}
+          />
+
+        </OrderDetailsContainer>
         <img src={avatarImg} alt="" />
       </section>
     </OrderCompletedContainer>

81ab24bf52746fc6986d782a79844aabd214be2f
Hugo Alves Varella <hugovarellaa@gmail.com>
2022-10-07 21:20:11 -0300

    Layout Checkout 50%

--- coffee-delivery/src/pages/Checkout/components/CheckoutForm/index.tsx
+++ coffee-delivery/src/pages/Checkout/components/CheckoutForm/index.tsx
@@ -1,7 +1,9 @@
-import { MapPinLine } from "phosphor-react";
+import { CurrencyDollar, MapPinLine } from "phosphor-react";
 import { useTheme } from "styled-components";
 import { TitleText } from "../../../../components/Typography";
 import { SectionTitle } from "../SectionTitle";
+import { AddressForm } from "./AddressForm";
+import { PaymentMethodOptions } from "./PaymentMethodOptions";
 import { CheckoutFormContainer, FormSectionContainer } from "./styles";
 
 export function CheckoutForm() {
@@ -17,6 +19,17 @@ export function CheckoutForm() {
           subtitle="Informe o endereço onde deseja receber seu pedido"
           icon={<MapPinLine size={22} color={colors["brand-yellow-dark"]} />}
         />
+
+        <AddressForm />
+      </FormSectionContainer>
+      <FormSectionContainer>
+        <SectionTitle
+          title="Pagamento"
+          subtitle="O pagamento é feito na entrega. Escolha a forma que deseja pagar"
+          icon={<CurrencyDollar size={22} color={colors["brand-purple"]} />}
+        />
+
+      <PaymentMethodOptions />
       </FormSectionContainer>
     </CheckoutFormContainer>
   )
